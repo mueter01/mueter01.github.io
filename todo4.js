d692b5cf60d06ed43afd70fa2d2b632cf9a6ea07
mueter01 <mueter01@luther.edu>
2015-10-19 12:55:51 -0500

    Update todo4.js

--- todo4.js
+++ todo4.js
@@ -1,1 +1,19 @@
+var input, priority;
+input = document.body.querySelector("#input");
+priority = document.body.querySelector("#priority");
 
+newTask = function() {
+  var newLi, text, box;
+  newLi = document.createElement("li");
+  text = document.createTextNode("input");
+  box = document.createElement("checkbox");
+  document.getElementById("newLi").className=priority;
+  newLi.appendChild(text);
+  newLi.appendChild(box);
+}
+
+addNewTask = function(newTask) {
+  var taskList;
+  taskList = document.body.querySelector("#list");
+  taskList.appendChild(newTask());
+}
